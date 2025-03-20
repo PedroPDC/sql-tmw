91e02478abec2145c0e0d2e9cf8bc056ae1ca9e4
Pedro Paulo <0901dantaspedro@gmail.com>
2025-03-20 18:41:16 -0300

    fix: Correção do professor

--- aula01/exercicios/ex01.sql
+++ aula01/exercicios/ex01.sql
@@ -1,7 +1,10 @@
 -- Quantos produtos temos da categoria 'artes'?
 
 SELECT
-    count(product_id) as qtd_produtos_artes
+    count(*) as qtd_linhas,
+    count(product_id) as qtd_produtos,
+    count(DISTINCT product_id) as qtd_produtos_distintos,
+    count(DISTINCT product_category_name) as qtd_categorias_distintas
 
 FROM tb_products
 
