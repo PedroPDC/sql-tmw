31cea8928950eee75bdaab6f6fefbef70ba35c12
Pedro Paulo <0901dantaspedro@gmail.com>
2025-03-20 19:04:51 -0300

    fix: Correção do professor

--- aula01/exercicios/ex03.sql
+++ aula01/exercicios/ex03.sql
@@ -1,11 +1,7 @@
 -- Crie uma coluna nova que contenha a informação de volume em m3
 
 SELECT
-    product_id,
-    product_category_name,
-    product_length_cm,
-    product_width_cm,
-    product_height_cm,
-    ((product_length_cm / 100) * (product_width_cm / 100) * (product_height_cm / 100)) as volume_m3
+    *,
+    product_length_cm * product_height_cm * product_width_cm / 1000000 as product_volume_m3
 
 FROM tb_products
