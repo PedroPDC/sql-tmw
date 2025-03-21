e22a64928b24721027e91cb976f3876863a630d8
Pedro Paulo <0901dantaspedro@gmail.com>
2025-03-21 20:53:47 -0300

    add: Select distinct com CASE WHEN

---
+++ aula03/distinct_case.sql
@@ -0,0 +1,8 @@
+SELECT
+    DISTINCT CASE WHEN product_category_name IS NULL THEN 'outros'
+                  ELSE product_category_name 
+             END AS categoria_fillna
+
+FROM tb_products
+
+order by 1
